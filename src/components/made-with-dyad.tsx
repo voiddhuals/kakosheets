0cffe13ba3541384380e2f085e98633e29c55e74
voiddhuals <vqne13377@gmail.com>
2026-01-02 20:52:51 +0100

    Update made-with-dyad.tsx

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -10,7 +10,7 @@ export const MadeWithDyad = () => {
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        {t("madeByVoid")}
+        {t("MadeByVoidd")}
       </a>
     </div>
   );
